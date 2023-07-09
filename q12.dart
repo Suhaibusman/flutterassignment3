ef7e7445d5532234a369d6df677642feafc15ca5
Suhaibusman <suhaibusman54@gmail.com>
2023-07-09 12:17:07 -0700

    assignment3

--- q12.dart
+++ q12.dart
@@ -14,7 +14,7 @@ void main(){
   for (var i = 1; i <= rows; i++) {
     for (var j = 1; j <= i; j++) {
       stdout.write(j);
-      
+    
     }
     print('');
   }
---
+++ q13.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+int row =4;
+for (var i = 1; i <=row; i++) {
+  for(var j =1;j <=i ;j++){
+
+    stdout.write(i);
+  }
+  print("");
+}
+
+
+}
